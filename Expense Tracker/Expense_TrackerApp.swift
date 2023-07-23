6857bb9e962c2b94ed5e4cef77e97a0ae6d41655
Konstantin Kochetkov <ka4ah4uk@yandex.ru>
2023-07-23 17:58:43 +0300

    Initial

---
+++ Expense Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,15 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense Tracker
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
